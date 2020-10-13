bb023af85c936b5b5ed7fd947b3ed3b749e607a3
Yamin Alam Sarker <yaminalam27@gmail.com>
2020-10-13 18:49:02 +0600

    package.json add

--- main.js
+++ main.js
@@ -2,7 +2,29 @@ var http = require('http');
 
 var server = http.createServer(function (req, res){
 
-    res.end('Hello world')
+    // res.end('Hello world')
+
+    if (req.url == "/"){
+
+        res.writeHead(200, {'Context-Type': 'text-html'})
+        res.write('<h1>This is Home page </h1>')
+        res.end();
+
+    }
+    else if (req.url == "/about"){
+
+        res.writeHead(200, {'Context-Type': 'text-html'})
+        res.write('<h1>This is about page </h1>')
+        res.end();
+
+    }
+    else if(req.url == "/contact"){
+
+        res.writeHead(200, {'Context-Type': 'text-html'})
+        res.write('<h1>This is contact page</h1>')
+        res.end();
+
+    }
 
 });
 
