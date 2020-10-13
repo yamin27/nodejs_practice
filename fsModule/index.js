e0ddf7fa186c03ba7b925d6f76f1d66321fb5a1d
Yamin Alam Sarker <yaminalam27@gmail.com>
2020-10-13 21:30:12 +0600

    FIle Rename Async

--- fsModule/index.js
+++ fsModule/index.js
@@ -6,21 +6,31 @@ var server = http.createServer(function (req,res){
     if (req.url == "/"){
 
 
-        fs.writeFile('demo.txt', 'file write', function (error){
 
-           if (error){
-               res.writeHead(200, {'Content-Type': 'text/html'});
-               res.write("File Write Fail");
-               res.end();
-           }
-           else {
-               res.writeHead(200, {'Content-Type': 'text/html'});
-               res.write("File Write Successfully");
-               res.end();
+        // fs.writeFile('demo.txt', 'file write', function (error){
 
-           }
+        //async rename
+        fs.rename('demo.txt', 'demo_rename.txt', function (error){
 
-        });
+            if (error){
+                res.writeHead(200, {'Content-Type': 'text/html'});
+                res.write("File Rename Fail");
+                res.end();
+            }
+            else {
+                res.writeHead(200, {'Content-Type': 'text/html'});
+                res.write("File Rename Successfully");
+                res.end();
+
+            }
+
+        })
+
+        // let error = fs.writeFileSync('Demo2.txt', 'Sync FIle Write Example');
+
+
+
+        // });
 
 
         //async read
